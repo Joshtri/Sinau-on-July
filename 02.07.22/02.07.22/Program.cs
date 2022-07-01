ffa711e19b35397edc5271d3145cd9fc4e76030d
Joshtri <stuffofyos1516@gmail.com>
2022-07-02 06:53:48 +0700

    Mulai sinau yuk

---
+++ 02.07.22/02.07.22/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._07._22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
